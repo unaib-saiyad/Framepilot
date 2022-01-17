fdafed9c641f048d492f5621289ea6dcea903e07
unaib-saiyad <unaibsaiyad18@gmail.com>
2022-01-17 19:37:40 +0530

    Database setup

--- FamepilotAssignment/FamepilotAssignment/settings.py
+++ FamepilotAssignment/FamepilotAssignment/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-f(ry=1=w)$j&l^4x!$5y&9)@3flxb(8+320x*-09nzy+&&y55a
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -75,17 +73,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FamepilotAssignment.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dc2efjhvabs85m',
+        'USER': 'uorjoyipdkohcu',
+        'PASSWORD': '6093b4ef673626a4becb39fb12d9262f7fc85442ed881a5a91e3c4eb51f77776',
+        'HOST': 'ec2-23-23-219-25.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -105,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -116,7 +115,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
